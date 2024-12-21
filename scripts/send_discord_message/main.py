396be34438eaa170d5b0d61a829cb650c2c06705
xezv <xezv@users.noreply.github.com>
2024-12-21 23:58:28 +0900

    .

--- scripts/send_discord_message/main.py
+++ scripts/send_discord_message/main.py
@@ -5,61 +5,59 @@ import os
 
 from serialize_fields import serialize_fields
 
-webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
-title = os.getenv('TITLE')
-description = os.getenv('DESCRIPTION')
-status = os.getenv('STATUS')
-actor = os.getenv('ACTOR')
-fields = os.getenv('FIELDS') or ''
-components = os.getenv('COMPONENTS') or None
-debug = os.getenv('DEBUG') == 'true' or False
+webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+title = os.getenv("TITLE")
+description = os.getenv("DESCRIPTION")
+status = os.getenv("STATUS")
+actor = os.getenv("ACTOR")
+fields = os.getenv("FIELDS") or ""
+components = os.getenv("COMPONENTS") or None
+debug = os.getenv("DEBUG") == "true" or False
 
 if debug:
-   print(f"Webhook URL: {webhook_url}")
-   print(f"Title: {title}")
-   print(f"Description: {description}")
-   print(f"Status: {status}")
-   print(f"Actor: {actor}")
-   print(f"Fields: {fields}")
-   print(f"Components: {components}")
-   debug = True
+    print(f"Webhook URL: {webhook_url}")
+    print(f"Title: {title}")
+    print(f"Description: {description}")
+    print(f"Status: {status}")
+    print(f"Actor: {actor}")
+    print(f"Fields: {fields}")
+    print(f"Components: {components}")
+    debug = True
 
 if status == "success":
-   color = 3066993
+    color = 3066993
 else:
-   color = 15158332
+    color = 15158332
 
 footer_text = f"Created by @{actor} • %{{now('%Y/%m/%d %H:%M')}}"
 footer_icon_url = f"https://github.com/{actor}.png"
 
 
 payload = {
-   "embeds": [{
-       "title": title,
-       "description": description,
-       "color": color,
-        "footer": {
-           "text": footer_text,
-          "icon_url": footer_icon_url
+    "embeds": [
+        {
+            "title": title,
+            "description": description,
+            "color": color,
+            "footer": {"text": footer_text, "icon_url": footer_icon_url},
         }
-   }],
+    ],
 }
 
 if fields:
-   payload['embeds'][0]['fields'] = serialize_fields(fields)
+    payload["embeds"][0]["fields"] = serialize_fields(fields)
 
 # if components:
 #    payload["components"] = json.loads(components)
 
-headers = {'Content-Type': 'application/json'}
-body = {
-   "content": json.dumps(payload),
-}
-response = requests.post(webhook_url, data=body, headers=headers)
+headers = {"Content-Type": "application/json"}
+data = json.dumps(payload)
 
-print(body)
+response = requests.post(webhook_url, data=data, headers=headers)
+
+print(data)
 if response.status_code == 204:
- print("Message sent to Discord successfully!")
+    print("Message sent to Discord successfully!")
 else:
- print(f"Failed to send message to Discord. Status code: {response.status_code}")
- print(response.text)
+    print(f"Failed to send message to Discord. Status code: {response.status_code}")
+    print(response.text)
